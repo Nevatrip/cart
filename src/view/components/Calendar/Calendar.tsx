8920a8cbabc66fb02c7d20de4ad7639abccd5e1d
Realetive <realetive@yandex.ru>
2019-02-05 16:37:36 +0300

    Add minDate to Calendar

--- src/view/components/Calendar/Calendar.tsx
+++ src/view/components/Calendar/Calendar.tsx
@@ -3,26 +3,27 @@ import ReactCalendar from 'react-calendar'
 
 export class Calendar extends React.Component<any, any> {
   constructor (props: any) {
-    super(props)
-    this.getData = this.getData.bind(this)
+    super(props);
+
     this.setState = this.setState.bind(this)
   }
+
   state = {
     selected: undefined,
     visible: false,
     dates: new Set(),
   }
+
   onChange (selected: any) {
     console.log(selected)
     this.setState({ selected })
   }
-  getData () {
+
+  componentDidMount () {
     const dates = this.props.dates ? new Set(this.props.dates) : new Set();
     this.setState({ dates });
   }
-  componentDidMount () {
-    this.getData()
-  }
+
   toTwoCharacterFormat (value: number) {
     if (value >= 10) {
       return String(value)
@@ -30,6 +31,7 @@ export class Calendar extends React.Component<any, any> {
       return `0${value}`
     }
   }
+
   dateToApiFormat (date: any) {
     if (date) {
       return `${this.toTwoCharacterFormat(date.getDate())}.${this.toTwoCharacterFormat(date.getMonth() + 1)}.${String(date.getFullYear())}`
@@ -37,6 +39,7 @@ export class Calendar extends React.Component<any, any> {
       return ''
     }
   }
+
   render() {
     return (
       <>
@@ -49,6 +52,7 @@ export class Calendar extends React.Component<any, any> {
         />
         {this.state.visible && (
           <ReactCalendar
+            minDate={new Date()}
             onChange={date => this.onChange(date)}
             value={this.state.selected}
             tileDisabled={({ date }) => {
