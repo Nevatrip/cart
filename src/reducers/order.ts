7c4d595828851137dcde1c20c0f64e9184ff460d
Nikolay Voevodin <nicolai.v.95@gmail.com>
2019-02-21 02:14:30 +0300

    Correct data conversion in storage

--- src/reducers/order.ts
+++ src/reducers/order.ts
@@ -6,16 +6,19 @@ export interface orderState {
 
 export const orderReducer = function (state: any = {orders: []}, action: any) {
     if (action.type === "SERVICE_UPDATE") {
-        if (Object.keys(action.payload.tickets).length) {
-            return Object.assign({}, state, {
-                orders: state.orders.concat([{
-                    title: action.payload.service.title.ru.name,
-                    date: new Date(action.payload.order.date),
-                    isOpenDate: false,
-                    isOpenTime: false,
-                    tickets: Object.values(action.payload.tickets)
-                }])
-            })
+        if (Object.keys(action.payload.tickets).length && action.payload.order.date) {
+            const tickets = Object.values(action.payload.tickets).filter((item: any) => (item.count))
+            const orders = state.orders.filter((item: any) => (item.title !== action.payload.service.title.ru.name)).concat([{
+                title: action.payload.service.title.ru.name,
+                date: new Date(action.payload.order.date),
+                isOpenDate: false,
+                isOpenTime: false,
+                tickets
+            }])
+            return {
+                ...state,
+                orders
+            }
         }
     }
     return state
