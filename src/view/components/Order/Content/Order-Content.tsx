06ced03bfb372a66cbc726ca228fb6f263d6c4c2
Realetive <realetive@yandex.ru>
2019-03-05 13:37:10 +0300

    Add styled example

--- src/view/components/Order/Content/Order-Content.tsx
+++ src/view/components/Order/Content/Order-Content.tsx
@@ -7,8 +7,39 @@ import {IOrderProps} from '../index'
 import TextField, { TextFieldProps } from '@material-ui/core/TextField';
 import {Fieldset} from "../../Fieldset/Fieldset";
 import {FormField} from "../../FormField/FormField";
+import { withStyles } from '@material-ui/core/styles';
 
 const cnOrder = cn("Order");
+
+const styles = {
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+};
+
+const StyledTextField = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(TextField);
 
 export class OrderContent extends React.Component<IOrderProps, {}> {
   constructor(props: IOrderProps) {
@@ -38,7 +69,7 @@ export class OrderContent extends React.Component<IOrderProps, {}> {
       <div className={cnOrder("Content")}>
         <Fieldset title="Персональные данные">
           <FormField title="Ваш email">
-            <TextField placeholder="info@email.com" type="email" onChange={this.handleEmail} />
+            <StyledTextField placeholder="info@email.com" type="email" onChange={this.handleEmail} />
           </FormField>
           <FormField title="Ваше имя и&nbsp;фамилия">
             <TextField placeholder="Иван Иванов" type="text" onChange={this.handleName} />
