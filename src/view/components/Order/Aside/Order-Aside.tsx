4ea2afc2d4229d99ec0a6575d2cf0cd0a89628e4
kizoso <kizoso@bk.ru>
2019-03-05 18:13:39 +0300

    stylization

--- src/view/components/Order/Aside/Order-Aside.tsx
+++ src/view/components/Order/Aside/Order-Aside.tsx
@@ -10,49 +10,69 @@ const cnOrder = cn('Order');
 
 const OrderAsideClass: React.FunctionComponent<{order: ICompileTicket[]}> = ({order}) => (
   <div className={cnOrder('Aside')}>
-    <blockquote>Sidebar</blockquote>
-    <div className={cnOrder('Tickets')}>
-      <blockquote>Ticket's preview</blockquote>
-      <ul>
-        {
-          order.map( orderItem => {
-            return (
-              <li key={orderItem.title}>
-                <h4>{orderItem.title}</h4>
-                <dl>
-                  {orderItem.date && <div><dt>Дата</dt><dd>{orderItem.isOpenDate ? 'isOpenDate' : orderItem.date.toLocaleDateString()}</dd></div>}
-                  {orderItem.time && <div><dt>Время</dt><dd>{orderItem.isOpenTime ? 'isOpenTime' : orderItem.time.toLocaleTimeString()}</dd></div>}
-                  {orderItem.point && <div><dt>Причал</dt><dd>[{orderItem.point.title}]({orderItem.point.map})</dd></div>}
-                  {orderItem.tickets && (
-                    <div>
-                      <dt>Билет</dt>
-                      <dd>
-                        <dl>
-                          {
-                            orderItem.tickets.map(ticket => (
-                              <div key={JSON.stringify(ticket)}>
-                                <dt>{ticket.ticket.title} ({ticket.category.title}), {ticket.count}x {ticket.price}р.</dt>
-                              </div>
-                            ) )
-                          }
-                        </dl>
-                      </dd>
+    <div className={cnOrder('Aside-Fixed')}>
+      {/*<blockquote>Sidebar</blockquote>*/}
+      <div className={cnOrder('Tickets')}>
+        {/*<blockquote>Ticket's preview</blockquote>*/}
+        <ul className={cnOrder('Tickets-Ul')}>
+          {
+            order.map( orderItem => {
+              return (
+                <li key={orderItem.title} className={cnOrder('Tickets-Li')}>
+                  <h4 className={cnOrder('Tickets-Title')}>{orderItem.title}</h4>
+                  <dl>
+                    {orderItem.date && <div className={cnOrder('Tickets-Row')}>
+                      <dt className={cnOrder('Tickets-Row-Title')}>Дата:</dt>
+                      <dd className={cnOrder('Tickets-Row-Content')}>{orderItem.isOpenDate ? 'isOpenDate' : orderItem.date.toLocaleDateString()}</dd>
+                    </div>}
+                    {orderItem.time && <div className={cnOrder('Tickets-Row')}>
+                      <dt className={cnOrder('Tickets-Row-Title')}>Время:</dt>
+                      <dd className={cnOrder('Tickets-Row-Content')}>{orderItem.isOpenTime ? 'isOpenTime' : orderItem.time.toLocaleTimeString()}</dd>
+                    </div>}
+                    {orderItem.point && <div className={cnOrder('Tickets-Row')}>
+                      <dt className={cnOrder('Tickets-Row-Title')}>Причал:</dt>
+                      <dd className={cnOrder('Tickets-Row-Content')}>[{orderItem.point.title}]({orderItem.point.map})</dd>
+                    </div>}
+                    {orderItem.tickets && (
+                      <div className={cnOrder('Tickets-Row')}>
+                        <dt className={cnOrder('Tickets-Row-Title')}>Билет:</dt>
+                        <dd className={cnOrder('Tickets-Row-Content')}>
+                          <dl className={cnOrder('Tickets-Col-Inner')}>
+                            {
+                              orderItem.tickets.map(ticket => (
+                                <div key={JSON.stringify(ticket)}>
+                                  <dt className={cnOrder('Tickets-Row-Inner')}>
+                                    <span className={cnOrder('Tickets-Row-Inner-El')}>{ticket.count}</span>
+                                    <span className={cnOrder('Tickets-Row-Inner-El')}>
+                                      <span className={cnOrder('Tickets-Row-Inner-El-Sm')}>{ticket.ticket.title} </span>
+                                      <i className={cnOrder('Tickets-Row-Inner-El-Xsm')}>{ticket.category.title}</i>
+                                    </span>
+                                    <span className={cnOrder('Tickets-Row-Inner-El')}>{ticket.price}р.</span>
+                                  </dt>
+                                </div>
+                              ) )
+                            }
+                          </dl>
+                        </dd>
+                      </div>
+                    )}
+                    <div className={cnOrder('Tickets-Row')}>
+                      <dt>Стоимость</dt><dd></dd>
                     </div>
-                  )}
-                  <div><dt>Стоимость</dt><dd></dd></div>
-                </dl>
-              </li>
-            )
-          } )
-        }
-      </ul>
+                  </dl>
+                </li>
+              )
+            } )
+          }
+        </ul>
+      </div>
+      <form className={cnOrder('Footer')} action="https://money.yandex.ru/eshop.xml" method="post">
+        <blockquote>Итого к оплате: {order.reduce((accum, curr) => {
+          return accum + (curr && curr.tickets ? curr.tickets.reduce((accum, curr) => (accum + ((curr.count ? curr.count : 0) * Number(curr.price))), 0) : 0)
+        }, 0)}, promocode, submit</blockquote>
+        <ButtonMUI>Оплатить</ButtonMUI>
+      </form>
     </div>
-    <form className={cnOrder('Footer')} action="https://money.yandex.ru/eshop.xml" method="post">
-      <blockquote>Sum: {order.reduce((accum, curr) => {
-        return accum + (curr && curr.tickets ? curr.tickets.reduce((accum, curr) => (accum + ((curr.count ? curr.count : 0) * Number(curr.price))), 0) : 0)
-      }, 0)}, promocode, submit</blockquote>
-      <ButtonMUI>Оплатить</ButtonMUI>
-    </form>
   </div>
 )
 
--- src/view/components/Order/Content/Order-Content.tsx
+++ src/view/components/Order/Content/Order-Content.tsx
@@ -7,39 +7,23 @@ import {IOrderProps} from '../index'
 import TextField, { TextFieldProps } from '@material-ui/core/TextField';
 import {Fieldset} from "../../Fieldset/Fieldset";
 import {FormField} from "../../FormField/FormField";
-import { withStyles } from '@material-ui/core/styles';
 
 const cnOrder = cn("Order");
 
-const styles = {
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+const textFieldStyles = {
+  textField: {
+    position: 'relative',
   },
-  label: {
-    textTransform: 'capitalize',
-  },
+  input: {
+    background: '#FFFFFF',
+    border: '1px solid #C4C4C4',
+    borderRadius: '3px',
+    width: '235px',
+    height: '40px',
+    fontSize: '14px',
+    letterSpacing: '0.04em',
+  }
 };
-
-const StyledTextField = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
-})(TextField);
 
 export class OrderContent extends React.Component<IOrderProps, {}> {
   constructor(props: IOrderProps) {
@@ -69,13 +53,26 @@ export class OrderContent extends React.Component<IOrderProps, {}> {
       <div className={cnOrder("Content")}>
         <Fieldset title="Персональные данные">
           <FormField title="Ваш email">
-            <StyledTextField placeholder="info@email.com" type="email" onChange={this.handleEmail} />
+            <TextField
+              InputProps={{style: textFieldStyles.input}}
+              placeholder="info@email.com"
+              type="email"
+              onChange={this.handleEmail} />
           </FormField>
           <FormField title="Ваше имя и&nbsp;фамилия">
-            <TextField placeholder="Иван Иванов" type="text" onChange={this.handleName} />
+            <TextField
+              InputProps={{style: textFieldStyles.input}}
+              placeholder="Иван Иванов"
+              type="text"
+              onChange={this.handleName} />
           </FormField>
           <FormField title="Ваш телефон">
-            <TextField placeholder="+7 (9__) ___-__-__" type="tel" onChange={this.handlePhone} />
+            <TextField
+              InputProps={{style: textFieldStyles.input}}
+              placeholder="+7 (9__) ___-__-__"
+              type="tel"
+              onChange={this.handlePhone}
+            />
           </FormField>
         </Fieldset>
         <ul className={cnOrder("List")}>
