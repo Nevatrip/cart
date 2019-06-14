5aabb4c96522475afc57feddd6fc0c1432f53944
legiorex <batyushev@gmail.com>
2019-06-15 00:30:47 +1000

    create counter

--- source/components/Counter/index.js
+++ source/components/Counter/index.js
@@ -5,13 +5,19 @@ const Counter = () => {
     const initialState = {
         count: 0,
     };
-    const [state, setstate] = useState(initialState);
-    
-
-    console.log('useState', state);
+    const [state, _setState] = useState(initialState);
+    const _increment = () => _setState({ count: state.count + 1 });
+    const _decrement = () => {
+        return state.count === 0 ? null : _setState({ count: state.count - 1 });
+    };
 
     return (
-        <div>test</div>
+        <>
+
+            <div>Количество билетов: {state.count}</div>
+            <button onClick = { _increment } >+</button>
+            <button onClick = { _decrement }>-</button>
+        </>
     );
 };
 
