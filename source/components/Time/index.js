c54860b55a1656a029e06909d16dcb53a1848a3b
Realetive <realetive@yandex.ru>
2019-06-26 19:14:13 +0300

    Add helper for fix timezone offset

--- source/components/Time/index.js
+++ source/components/Time/index.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import { format } from 'date-fns';
 import useStoreon from 'storeon/react';
+import { getActualTime } from '../../instruments/helpers';
 
 export const Time = (props) => {
-
   const { dispatch, totalData } = useStoreon('totalData');
   const { timesAll, productKey } = props;
+  const currentItem = totalData[productKey];
 
-  // console.log('productKey', productKey);
-  // console.log('timesAll', timesAll);
-
-  if (totalData === {}) {
+  if (totalData === {} || timesAll.length === 0) {
     return null;
   }
   if (timesAll.length === 0) {
@@ -28,36 +26,24 @@ export const Time = (props) => {
     dispatch('totalData/updateCart', currentItem);
   };
 
-  const renderTimes =  timesAll.map((item, index) => {
-    console.log('item', item);
-
-    const timeOffset = new Date().getTimezoneOffset();
-    const time = new Date(item.startLabel);
-
-    /* Время на  бэкенде хранится в UTC. При создании объекта даты
-     * он автоматически «наследует» локальный часовой пояс. Чтобы
-     * сбросить его, добавляем к получившемуся времени это смещение
-     * (`timeOffset`) и устанавливаем время часового пояса по умолчанию —
-     * GTM +3, т. е. + 180 минут
-     */
-    time.setMinutes(time.getMinutes() + timeOffset + 180);
+  const renderTimes = timesAll.map(({ _key, startLabel }, index) => {
+    const time = getActualTime(new Date(startLabel));
 
     return (
-      <li data-key = { item._key } key = { item._key }>
+      <li data-key = { _key } key = { _key }>
         <label>
           <input
-            checked = { currentItem.selectTimeKey ? currentItem.selectTimeKey === item._key : index === 0 }
-            data-time = { item.start }
+            checked = {
+              currentItem.selectTimeKey
+                ? currentItem.selectTimeKey === _key
+                : index === 0
+            }
+            data-time = { time }
             name = { `time-${productKey}` }
             type = 'radio'
-            value = { item._key }
-            onChange = { _changeTime }
+            value = { _key }
           />
-          {format(
-            time,
-            'HH:mm',
-            new Date()
-          )}
+          {format(time, 'HH:mm', new Date())}
         </label>
       </li>
     );
